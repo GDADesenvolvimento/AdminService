3a1f6b65be3030f2eae269000b3874e6ce332c55
Diogo Bemfica <diogo.fragabemfica@gmail.com>
2016-07-07 19:58:21 -0300

    Correção nos migrations

--- database/migrations/2016_07_05_231620_create_services_table.php
+++ database/migrations/2016_07_05_231620_create_services_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateDomainsTable extends Migration
+class CreateServicesTable extends Migration
 {
     /**
      * Run the migrations.
